286291d05900f2aa66d1c5365416873c82e9f60d
coderamitdey <amitdey7150@gmail.com>
2025-09-30 09:07:46 +0600

    first commit

---
+++ src/Components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Countries are Loading...</h1>
+        </div>
+    );
+};
+
+export default Countries;
